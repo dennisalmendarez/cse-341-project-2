1b13310e87f555f19794835230b827cacd3031dc
dennisalmendarez <dennisyassid2212@hotmail.com>
2025-01-27 19:16:55 -0500

    fix

--- swagger.js
+++ swagger.js
@@ -2,11 +2,11 @@ const swaggerAutogen = require('swagger-autogen')()
 
 const doc = {
     info: {
-        title: "animes Api",
-        description: "Animes Api",
+        title: "movies and animes Api",
+        description: "API for managing movies and animes.",
     },
     host: "localhost:3001",
-    schemes: ['http'],
+    schemes: ['https'],
     };
 
 const outputFile = './swagger.json';
