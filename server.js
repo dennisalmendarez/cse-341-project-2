7c2de1bd25da660f7a1bf04a948ef2f238a6be1a
dennisalmendarez <dennisyassid2212@hotmail.com>
2025-02-02 16:51:14 -0500

    fix

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(cors({ origin: 'https://cse-341-project-2-j6y0.onrender.com', credentials: true, methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'] }));
+app.use(cors({ origin: '*', credentials: true, methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'] }));
 app.use('/' , require('./routes'));
 
 passport.use(new GithubStrategy({
