269dd9d3eb091d128ac63fb2f5a0d72739bc2767
dennisalmendarez <dennisyassid2212@hotmail.com>
2025-02-06 20:12:46 -0500

    ds

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,5 +1,6 @@
 const isAuthenticated = (req, res, next) => {
-    if (req.session.user === undefined) {
+    console.log('Session data:', req.session);  // Debugging output
+    if (!req.session.user) {
         return res.status(401).json({ message: 'You do not have access.' });
     }
     next();
